57954b3da4270d98e75d39950d163be1f886f390
devs <devs>
2016-05-28 21:50:35 +0200

    Configuration Hibernate succesful

--- SS2I/src/main/java/com/mehdi/ss2i/HomeController.java
+++ SS2I/src/main/java/com/mehdi/ss2i/HomeController.java
@@ -2,14 +2,19 @@ package com.mehdi.ss2i;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mehdi.beans.Developpeur;
 
 /**
  * Handles requests for the application home page.
@@ -24,14 +29,31 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx =null;
+		
+		try
+		{
+			tx = (Transaction) session.beginTransaction();
+
+		
+			
+	
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			if (tx != null) {
+			    tx.rollback();
+			}
+		}
+		finally {
+			session.close();
+		}
 		
 		return "home";
 	}
